b1ae834245f65aa593d846a783c992d83d7a5a24
gian0202 <gianfloreal@hotmail.com>
2022-03-08 20:02:49 -0300

    Create variavel.php

---
+++ variavel.php
@@ -0,0 +1,9 @@
+<?php
+//Como criar uma variavel e fazer ela aparecer na tela 
+$nome  = "Gian";
+
+echo $nome . "<br>";
+// para mostrar qual o tipo de dado da variavel na tela: 
+var_dump($nome)
+
+?>
